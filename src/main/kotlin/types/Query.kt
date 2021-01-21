a607ad2f21a704be0700810bbd6d9a055527e34f
kk-mats <k.mats.kk@gmail.com>
2021-01-21 23:02:18 +0900

    fix query format

--- src/main/kotlin/types/Query.kt
+++ src/main/kotlin/types/Query.kt
@@ -3,10 +3,7 @@ package com.github.kk_mats.ccx_plugin_detect_ccfsw.types
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Detector(val name: String, val id: String, val version: String)
-
-@Serializable
 data class Target(val revision: String, val directory: String)
 
 @Serializable
-data class Query(val detector: Detector, val targets: List<Target>, val parameters:  Parameters)
+data class Query(val detectorVersion: String, val targets: List<Target>, val parameters:  Parameters)
